5bf9dc653116e29f326163ca737163642670a88e
Boddu Venkateswara Rao <venkateshboddu923@gmail.com>
2026-07-16 15:08:12 +0530

    Delete app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from .schemas.auth import *
-from .schemas.post import *
-from .schemas.user import *
-
-__all__ = [
-    "UserCreate", "UserResponse", "UserUpdate",
-    "LoginRequest", "Token",
-    "PostCreate", "PostResponse"
-]
